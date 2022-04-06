1a4e455801b209edf3dd1a2ec2d888941108ce1a
wlQkr <wlqkr1010@gmail.com>
2022-04-06 23:23:45 +0900

    Initial Commit

---
+++ SwiftUIParctice/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftUIParctice
+//
+//  Created by 박지용 on 2022/04/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        MyView(helloFont: .title)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIParctice/SwiftUIParcticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIParcticeApp.swift
+//  SwiftUIParctice
+//
+//  Created by 박지용 on 2022/04/06.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIParcticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
